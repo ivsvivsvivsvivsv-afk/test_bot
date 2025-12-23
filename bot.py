1ff18890ab93e937ed3cb91d25259440ef131c3b
ivsvivsvivsvivsv-afk <ivsvivsvivsvivsv@gmail.com>
2025-12-23 12:45:50 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -186,5 +186,5 @@ if __name__ == '__main__':
     except Exception as e:
         print(f"⚠️  Ошибка webhook: {e}")
     
-    port = int(os.getenv('PORT', 5000))
+    port = int(os.getenv('PORT', 8080))
     app.run(host='0.0.0.0', port=port, debug=False)
